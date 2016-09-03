0a8b9cda872cfb16155a26dda0b9927b24ab4f4f
Sergey <xxsmotur@gmail.com>
2016-09-04 00:28:01 +0500

    Implementation of check type

--- validder/validder.py
+++ validder/validder.py
@@ -6,7 +6,7 @@
 
 class Validder(object):
 	def __init__(self, schema):
-		self.schema = schema
+		self.input_schema = schema
 		if self._valid_input_schema(schema) is False:
 			return False
 
@@ -22,6 +22,25 @@ class Validder(object):
 
 		return True
 
+	def _check_type(self, properties):
+		''' checking type of the input value.
+		Allowed types: string, list, int, float, any
+		'''
+		allowed_types = ["string", "int", "float", "list"]
+		current_type = properties["type"]
+		return True if current_type in allowed_types else False
+
+
+	def _check_properties(self, key, value):
+		properties = self.input_schema[key]
+
+
 
 	def validate(self, schema):
-		pass
+		if isinstance(schema, dict) is False:
+			raise Exception("Unable to validate schema")
+
+		for key, value in schema.items():
+			if key not in self.input_schema:
+				raise Exception("{0} is not exist at the input schema".format(key))
+			properties = self.input_schema[key]
