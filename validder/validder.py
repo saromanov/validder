b3f2b23afc4f124750b768f114ea41d21b67491a
Sergey <xxsmotur@gmail.com>
2016-09-10 23:38:50 +0500

    CHanges at the validation of the type

--- validder/validder.py
+++ validder/validder.py
@@ -22,25 +22,28 @@ class Validder(object):
 
 		return True
 
-	def _check_type(self, properties):
+	def _check_type(self, properties, key, value):
 		''' checking type of the input value.
 		Allowed types: string, list, int, float, any
 		'''
 		allowed_types = ["string", "int", "float", "list"]
 		current_type = properties["type"]
-		return True if current_type in allowed_types else False
+		if not current_type in allowed_types: return False
+		current_value = self._check_type_value(current_type, value)
+		if current_value is False: return False
+		return True
 
 	def _check_type_value(self, typeItem, value):
 		''' return True if value satisfied key properties
 		'''
 		if typeItem == "string":
-			return True if isinstance(key, str) else False
+			return True if isinstance(value, str) else False
 		if typeItem == "int":
-			return True if isinstance(key, int) else False
+			return True if isinstance(value, int) else False
 		if typeItem == "float":
-			return True if isinstance(key, float) else False
+			return True if isinstance(value, float) else False
 		if typeItem == "list":
-			return True if isinstance(key, list) else False
+			return True if isinstance(value, list) else False
 		if typeItem == "any":
 			return True
 		return False
@@ -66,11 +69,8 @@ class Validder(object):
 			if key not in self.input_schema:
 				raise Exception("{0} is not exist at the input schema".format(key))
 			properties = self.input_schema[key]
-			value = self._check_type(properties)
+			value = self._check_type(properties, key, value)
 			if value is False:
-				return False
-			type_value = self._check_type_value(key, value)
-			if type_value is False:
 				return False
 			# at the last, check that elements is on required
 			if key in required:
@@ -79,3 +79,4 @@ class Validder(object):
 		# checking, that required is empty
 		if len(required) > 0:
 			raise Exception("Required elements {0} is not exist".format(required))
+		return True
